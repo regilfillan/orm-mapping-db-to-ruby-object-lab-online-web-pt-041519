b2ebad39da610d9997e94dc26257eb2fba07687e
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-13 20:16:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,6 +54,17 @@ class Student
       self.new_from_db(row)
       end
     end
+    
+  def self.first_X_students_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+      end
+  end
   
   def save
     sql = <<-SQL
