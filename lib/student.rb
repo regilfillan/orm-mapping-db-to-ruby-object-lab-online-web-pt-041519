03da9cd30ffa96798a95bb089753290f9ce6b9b0
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-13 20:18:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,7 +59,7 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = 10 LIMIT (number)
+      WHERE grade = 10 LIMIT ?
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
