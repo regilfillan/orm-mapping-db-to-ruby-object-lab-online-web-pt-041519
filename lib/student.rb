96ade7fcdc049404fa1d120230fd4fcc11dd3d1b
Ryan Gilfillan <regilfillan@gmail.com>
2019-07-13 20:19:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,7 @@ class Student
       WHERE grade = 10 
       ORDER BY students.id LIMIT ?
     SQL
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, number).map do |row|
       self.new_from_db(row)
       end
   end
